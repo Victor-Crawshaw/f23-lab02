835d7b956e420a2dd9cd963a97bd0e2bb059d7a0
Victor Crawshaw <vcrawsha@andrew.cmu.edu>
2024-01-23 21:38:21 -0500

    removed rectangle interface and implemented shape interface

--- typescript/src/index.ts
+++ typescript/src/index.ts
@@ -1,6 +1,17 @@
 import { newRenderer } from "./renderer.js"
-import { Rectangle, newRectangle } from "./shapes/rectangle.js";
+import { Shape } from "./shapes/shape.js";
+import { square } from "./shapes/square.js";
+import { circle } from "./shapes/circle.js";
+import { newRectangle } from "./shapes/rectangle.js";
 
-const rectangle: Rectangle = newRectangle(2, 3)
-const renderer = newRenderer(rectangle)
-renderer.draw();
+const my_rectangle: Shape = newRectangle(2, 3);
+const my_square: Shape = square(4);
+const my_circle: Shape = circle(5);
+
+const renderer1 = newRenderer(my_rectangle);
+const renderer2 = newRenderer(my_square);
+const renderer3 = newRenderer(my_circle);
+
+renderer1.draw();
+renderer2.draw();
+renderer3.draw(); 
