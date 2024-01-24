bcb6c8eeb4123a0c18dc98776b8225d718b23f28
Victor Crawshaw <vcrawsha@andrew.cmu.edu>
2024-01-23 21:40:43 -0500

    added extra shapes for testing

--- java/src/main/java/edu/cmu/cs/cs214/lab02/Main.java
+++ java/src/main/java/edu/cmu/cs/cs214/lab02/Main.java
@@ -1,14 +1,22 @@
 package edu.cmu.cs.cs214.lab02;
 
 import edu.cmu.cs.cs214.lab02.shapes.Rectangle;
+import edu.cmu.cs.cs214.lab02.shapes.Square;
+import edu.cmu.cs.cs214.lab02.shapes.Circle;
 import edu.cmu.cs.cs214.lab02.shapes.Shape;
 
 public class Main {
     public static void main(String[] args) {
         Shape rectangle = new Rectangle(2, 3);
+        Shape square = new Square(3);
+        Shape circle = new Circle(5);
 
-        Renderer renderer = new Renderer(rectangle);
+        Renderer renderer1 = new Renderer(rectangle);
+        Renderer renderer2 = new Renderer(square);
+        Renderer renderer3 = new Renderer(circle);
 
-        renderer.draw();
+        renderer1.draw();
+        renderer2.draw();
+        renderer3.draw();
     }
 }
